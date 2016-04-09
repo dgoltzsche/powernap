ca6db91bd9b18b7d5efc200d4a531f0b47178f43
Stefan Brenner <stefan@fam-brenner.eu>
2016-04-09 15:30:59 +0200

    switches to kilo units in config file; threaded networkmonitor does not block other monitors and can easily average the value over a few seconds.

--- powernap/powernap/monitors/NetworkMonitor.py
+++ powernap/powernap/monitors/NetworkMonitor.py
@@ -17,23 +17,36 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, re, subprocess
+import threading 
+import time
 from logging import error, debug, info, warn
 
 class NetworkMonitor():
 
     # Initialise
     def __init__(self, config):
-        print("NetworkMonitor starting...")
         self._type = config['monitor']
         self._name = config['name']
         self._absent_seconds = 0
         self._bwin = int(config['bwin'])
         self._bwout = int(config['bwout'])
         self._iface = config['iface']
+        # start background thread
+        self._active = False
+        self._lock = threading.Lock()
+        self._thread = threading.Thread(target=self.thread_main)
+        self._thread.start()
 
-    # Check for connections
-    def active(self):
-        ps = subprocess.getoutput("bwm-ng -o csv -c 1 -t 1000").splitlines()
+    def thread_main(self):
+        while True:
+            active = self.checkNetworkTraffic()
+            self._lock.acquire()
+            self._active = active
+            self._lock.release()
+            time.sleep(1)
+
+    def checkNetworkTraffic(self):
+        ps = subprocess.getoutput("bwm-ng -o csv -c 1 -t 3000").splitlines()
         for line in ps:
             columns = line.split(";")
             
@@ -41,11 +54,18 @@ class NetworkMonitor():
             if iface != self._iface: 
                 continue
 
-            bw_out = float(columns[2]) / 1024 / 1024 * 8
-            bw_in = float(columns[3]) / 1024 / 1024 * 8
+            bw_out = float(columns[2]) / 1024 * 8
+            bw_in = float(columns[3]) / 1024 * 8
             if bw_in > self._bwin or bw_out > self._bwout:
                 return True
         return False
+
+    # Check for connections
+    def active(self):
+        self._lock.acquire()
+        active = self._active
+        self._lock.release()
+        return active
 
     def start(self):
         pass
